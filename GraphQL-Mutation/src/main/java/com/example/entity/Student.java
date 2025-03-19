32c802715b50457cfd5fc3999d351c452599830a
Pedro Viveiros Ferreira <pferrevi@mercadona.com>
2025-03-19 12:32:02 +0000

    commit graphQL

---
+++ GraphQL-Mutation/src/main/java/com/example/entity/Student.java
@@ -0,0 +1,39 @@
+package com.example.entity;
+
+import com.example.request.CreateStudentRequest;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "student")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@OneToOne
+	@JoinColumn(name = "address_id")
+	private Address address;
+	
+	@OneToMany(mappedBy = "student", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Subject> learningSubjects;
+
+}
