0beb801f8191fb622a4a5abb6a1ddf00cb5b676d
Pedro Viveiros Ferreira <pferrevi@mercadona.com>
2025-04-02 09:18:25 +0200

    fix

--- GraphQL-RESTAPI/src/main/java/com/example/config/GraphQLConfiguration.java
+++ GraphQL-RESTAPI/src/main/java/com/example/config/GraphQLConfiguration.java
@@ -2,16 +2,10 @@ package com.example.config;
 
 import com.example.dataLoader.SubjectDataLoader;
 import lombok.RequiredArgsConstructor;
-import org.dataloader.DataLoader;
 import org.dataloader.DataLoaderFactory;
-import org.dataloader.DataLoaderOptions;
-import org.dataloader.DataLoaderRegistry;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.DataLoaderRegistrar;
-import org.springframework.graphql.execution.GraphQlSource;
-import org.springframework.stereotype.Component;
 
 @Configuration
 @RequiredArgsConstructor
