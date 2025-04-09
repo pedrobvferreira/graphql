3f044c92e8bdd9b323351c3a103ec11f6d22d251
Pedro Viveiros Ferreira <pferrevi@mercadona.com>
2025-04-09 11:17:29 +0100

    fix

---
+++ GraphQL-Dataloader/src/main/java/com/example/config/StudentDataLoaderInterceptor.java
@@ -0,0 +1,36 @@
+package com.example.config;
+
+import com.example.dataloader.AddressDataLoader;
+import com.example.dataloader.SubjectDataLoader;
+import graphql.GraphQLContext;
+import lombok.AllArgsConstructor;
+import org.dataloader.DataLoaderRegistry;
+import org.dataloader.DataLoaderFactory;
+import org.springframework.graphql.execution.DataLoaderRegistrar;
+import org.springframework.graphql.server.WebGraphQlInterceptor;
+import org.springframework.graphql.server.WebGraphQlRequest;
+import org.springframework.graphql.server.WebGraphQlResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@AllArgsConstructor
+public class StudentDataLoaderInterceptor implements WebGraphQlInterceptor {
+
+    private final AddressDataLoader addressDataLoader;
+    private final SubjectDataLoader subjectDataLoader;
+
+    @Override
+    public Mono<WebGraphQlResponse> intercept(WebGraphQlRequest request, Chain chain) {
+        // Cria DataLoaderRegistry por requisição
+        DataLoaderRegistry registry = new DataLoaderRegistry();
+        registry.register("addressDataLoader", DataLoaderFactory.newMappedDataLoader(addressDataLoader));
+        registry.register("subjectDataLoader", DataLoaderFactory.newMappedDataLoader(subjectDataLoader));
+
+        request.configureExecutionInput((executionInput, builder) ->
+                builder.dataLoaderRegistry(registry).build()
+        );
+
+        return chain.next(request);
+    }
+}
